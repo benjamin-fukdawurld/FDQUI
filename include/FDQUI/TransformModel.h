8ddea7e1d4460d0a937c5cfb370c01d36adf1fec
Benjamin ZAMOUR <benjamin.zamour-ext@faurecia.com>
2020-03-04 17:42:30 +0100

    Progressing on TransformModel

--- include/FDQUI/TransformModel.h
+++ include/FDQUI/TransformModel.h
@@ -2,17 +2,73 @@
 #define TRANSFORMMODEL_H
 
 #include <QAbstractItemModel>
+#include <FD3D/Utils/Transform.h>
 
 class TransformModel : public QAbstractItemModel
 {
     Q_OBJECT
     protected:
+        /*
+            mutable glm::mat4 m_mat;
+            glm::vec3 m_position;
+            glm::vec3 m_scale;
+            glm::quat m_rotation;
+            mutable bool m_isUpToDate;
+        */
 
+        enum class TransformFields: uint8_t
+        {
+            Position,
+            Scale,
+            Rotation,
+            Matrice,
+            IsUpToDate
+        };
+
+        static constexpr size_t NumberOfFields = 5;
+
+    protected:
+        FD3D::Transform *m_transform;
 
     public:
         TransformModel(QObject *parent = nullptr);
 
         ~TransformModel() override = default;
+
+        QModelIndex	index(int row,
+                          int column,
+                          const QModelIndex &parent = QModelIndex()
+            ) const override;
+
+        QModelIndex parent(const QModelIndex &index) const override;
+
+        int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+
+        int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+
+        bool hasChildren(const QModelIndex &parent = QModelIndex()) const override;
+
+        QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+        QVariant headerData(
+                int section,
+                Qt::Orientation orientation,
+                int role = Qt::DisplayRole
+            ) const override;
+
+        bool setData(
+                const QModelIndex &index,
+                const QVariant &value,
+                int role = Qt::EditRole
+            ) override;
+
+        Qt::ItemFlags flags(const QModelIndex &index) const override;
+
+        FD3D::Transform *getTranfsorm();
+        const FD3D::Transform *getTranfsorm() const;
+
+        void setTranfsorm(FD3D::Transform *transform);
+        void unsetTranfsorm() { setTranfsorm(nullptr); }
 };
 
 #endif // TRANSFORMMODEL_H
--- src/TransformModel.cpp
+++ src/TransformModel.cpp
@@ -1,7 +1,49 @@
 #include "include/FDQUI/TransformModel.h"
 
 TransformModel::TransformModel(QObject *parent) :
-    QAbstractItemModel(parent)
+    QAbstractItemModel(parent),
+    m_transform(nullptr)
 {
 
+}
+
+QModelIndex TransformModel::index(int row, int column, const QModelIndex &parent) const
+{
+    if(parent.isValid())
+    {
+        return createIndex(row, column, reinterpret_cast<void*>(m_transform));
+    }
+}
+
+int TransformModel::rowCount(const QModelIndex &parent) const
+{
+    if(!parent.isValid())
+        return NumberOfFields;
+
+    return 0;
+}
+
+int TransformModel::columnCount(const QModelIndex &parent) const
+{
+    if(!parent.isValid())
+        return 1;
+
+    return 0;
+}
+
+FD3D::Transform *TransformModel::getTranfsorm()
+{
+    return m_transform;
+}
+
+const FD3D::Transform *TransformModel::getTranfsorm() const
+{
+    return m_transform;
+}
+
+void TransformModel::setTranfsorm(FD3D::Transform *transform)
+{
+    emit beginResetModel();
+    m_transform = transform;
+    emit endResetModel();
 }
